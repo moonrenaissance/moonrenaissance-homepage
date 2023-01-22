c5e4f07f3172ff24f6d1e4226c53f863eed0e8c6
Daniel Clover <danil.word@mail.ru>
2023-01-22 21:54:16 +0500

    Initial Commit

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import {Box,
         Heading, 
         Text,
         Container, 
-        Devider, 
+        Divider, 
         Button} from '@chakra-ui/react'
 
 const NotFound = () => {
@@ -11,7 +11,7 @@ const NotFound = () => {
         <Container>
             <Heading as="h1" Text="Not found"/>
             <Text>The page you&#39;re looking for was not found.</Text>
-            <Devider my={6} />
+            <Divider my={6} />
             <Box my={6} align="center">
                 <Button as={NextLink} href="/" colorScheme="teal">
                     Return to Home
